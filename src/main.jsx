4cf2574c6e0a7faeb7abb6fa8a4b7678e37b3520
KurkaGetrurka <98816761+KurkaGetrurka@users.noreply.github.com>
2026-06-12 10:19:01 +0200

    Mount encrypted database launcher

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AestheticKanbanBoard from './App.jsx'
+import { SecurityDatabaseLauncher } from './kanban/components/security-database-launcher.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AestheticKanbanBoard />
+    <SecurityDatabaseLauncher />
   </React.StrictMode>,
 )
