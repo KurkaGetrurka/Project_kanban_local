08188a1d6ef3530a5bfa765e824ac7b1b1a67df3
KurkaGetrurka <98816761+KurkaGetrurka@users.noreply.github.com>
2026-06-12 11:45:50 +0200

    Show startup database gate on app launch

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AestheticKanbanBoard from './App.jsx'
 import { SecurityDatabaseLauncher } from './kanban/components/security-database-launcher.jsx'
+import { StartupDatabaseGate } from './kanban/components/startup-database-gate.jsx'
 import { LEGACY_KEYS, STORAGE_KEY } from './kanban/shared.jsx'
 import { installBrowserPersistenceGuard } from './kanban/security/browserStoragePolicy.js'
 import './index.css'
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AestheticKanbanBoard />
     <SecurityDatabaseLauncher />
+    <StartupDatabaseGate />
   </React.StrictMode>,
 )
