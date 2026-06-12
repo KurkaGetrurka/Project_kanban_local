cdc6e83b3d9704faba3e3a5b96edc58c4215f51c
KurkaGetrurka <98816761+KurkaGetrurka@users.noreply.github.com>
2026-06-12 10:31:06 +0200

    Install browser persistence guard on startup

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AestheticKanbanBoard from './App.jsx'
 import { SecurityDatabaseLauncher } from './kanban/components/security-database-launcher.jsx'
+import { LEGACY_KEYS, STORAGE_KEY } from './kanban/shared.jsx'
+import { installBrowserPersistenceGuard } from './kanban/security/browserStoragePolicy.js'
 import './index.css'
+
+installBrowserPersistenceGuard(STORAGE_KEY, LEGACY_KEYS)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
